8d1db447c1e74ab62e3afd42eeb5ca3c2c644269
AndreaVentur10 <55163240+AndreaVentur10@users.noreply.github.com>
2021-10-20 14:26:23 +0200

    Control PID y escena del profesor

--- muia-2021-client.py
+++ muia-2021-client.py
@@ -11,7 +11,7 @@ import sys
 import time
 
 # import cv2 as cv
-# import numpy as np
+#import numpy as np
 import sim
 
 # --------------------------------------------------------------------------
@@ -81,97 +81,124 @@ def getSonar(clientID, hRobot):
 def getImageBlob(clientID, hRobot):
     rc,ds,pk = sim.simxReadVisionSensor(clientID, hRobot[2],
                                          sim.simx_opmode_buffer)
+
     blobs = 0
     coord = []
     if rc == sim.simx_return_ok and pk[1][0]:
+
         blobs = int(pk[1][0])
         offset = int(pk[1][1])
         for i in range(blobs):
             coord.append(pk[1][4+offset*i])
             coord.append(pk[1][5+offset*i])
+            coord.append(pk[1][6+offset*i])
 
     return blobs, coord
 
 # --------------------------------------------------------------------------
 
 def avoid(sonar, blobs, coord):
-    
-    lspeed, rspeed = +1.0, +1.0
+
+    lspeed, rspeed = +0.7, +0.7
     auxl, auxr = +1.0, +1.0
     print(coord)
     if blobs == 1:
         if coord[0]<= 0.25:
-            rspeed += 1.5
+            rspeed += 1.2       #1.5
+            print("1")
         elif coord[0]>0.25 and coord[0]<= 0.5:
-            rspeed += 1.0
+            rspeed += 0.7       #1.0
+            print("12")
         elif coord[0]>0.5 and coord[0]<= 0.75:
-            lspeed += 1.0
+            lspeed += 0.7       #1.0
+            print("13")
         else : #coord[1]> 0.5:
-            lspeed += 1.5
+            lspeed += 1.2
+            print("14")
 
-            
         if coord[1]< 0.15:
-            lspeed -= 1.5
-            rspeed -= 1.5
+            #lspeed -= 1.2
+            lspeed = lspeed/3
+            #rspeed -= 1.2
+            rspeed = rspeed/3
+            print("25")
         elif coord[1]>=0.15 and coord[1]< 0.25:
-            lspeed -= 1.0
-            rspeed -= 1.0
+            #lspeed -= 1.0
+            lspeed = lspeed/2
+            #rspeed -= 1.0
+            rspeed = rspeed/2
+            print("26")
         elif coord[1]< 0.5 and coord[1]>= 0.25:
             lspeed += 0.25      ##0.5
             rspeed += 0.25       ##0.5
+            print("27")
         elif coord[1]>= 0.5 and coord[1]< 0.8:
             lspeed += 0.5  # 0.7
             rspeed += 0.5
+            print("28")
         else:
             lspeed += 0.8       ##1.1
             rspeed += 0.8       ##1.1
-         
+            print("29")
+
         #### obstacles ###############
+        print('ImageBlob:', coord[0],coord[1])
         print('centrales:', sonar[3], sonar[4])
         print('Lado Izq:', sonar[0], sonar[1], sonar[2])
         print('Lado Der:', sonar[5], sonar[6], sonar[7])
         print('Traseros:', sonar[8], sonar[9],sonar[10], sonar[11],sonar[12], sonar[13],sonar[14], sonar[15])
-        
-        if sonar[3]< 0.18 or sonar[4]< 0.18: ## ~0.15
+
+        if sonar[3]< 0.18 or sonar[4]< 0.18: ## ~0.15  ###AQUIII HAYYYY MOVIDAAAAAA
             #lspeed -= 3.0 ##2.0
             #rspeed -= 3.0
             if  coord[0] < 0.4:
                 lspeed = -0.2
                 rspeed = 1.2
+                print("30")
             elif coord[0] > 0.6:
                 lspeed = 1.2
                 rspeed = -0.2
+                print("31")
             else:
                 lspeed = 0.0
                 rspeed = 0.0
+                print("32")
         elif sonar[3] <0.25 and sonar[4] < 0.25 and coord[1]< 0.25:
-            lspeed -= 2.0 ##prueba
-            rspeed -= 2.0
+            lspeed -= 1.4 ##2.0
+            rspeed -= 1.4
+            print("43")
         elif sonar[3] < 0.5 and sonar[4] < 0.5 and coord[1]< 0.35: ##0.25
-            lspeed -= 1.5 ##1.0
-            rspeed -= 1.5
+            lspeed -= 1.2 ##1.5
+            rspeed -= 1.2
+            print("44")
         elif sonar[3] < 0.5 and sonar[4] < 0.5:
             lspeed -= 0.5
             rspeed -= 0.5
+            print("45")
         elif sonar[0]<0.7 and sonar[1] < 0.7:
-            lspeed += 1.5##2.0
+            lspeed += 1.4   ##1.5
+            print("46")
         elif sonar[6]<0.7 and sonar[7] < 0.7:
-            rspeed += 1.5
+            rspeed += 1.4   ##1.5
+            print("47")
         elif sonar[2]<0.7 and sonar[3] < 0.7:
-            lspeed += 1.5    
+            lspeed += 1.4   ##1.5
+            print("48")
         elif sonar[4]<0.7 and sonar[5] < 0.7:
-            rspeed += 1.5   
-            
+            rspeed += 1.4   ##1.5
+            print("49")
+
     else: #blobs != 1
-        lspeed, rspeed = +2.0, +0.0 #+0.0, +2.0
-        
+        lspeed, rspeed = +1.4, +0.0 #+0.0, +2.0
+        print("60")
+
         #### obstacles ###############
         print('centrales:', sonar[3], sonar[4])
         print('Lado Izq:', sonar[0], sonar[1], sonar[2])
         print('Lado Der:', sonar[5], sonar[6], sonar[7])
         print('Traseros:', sonar[8], sonar[9],sonar[10], sonar[11],sonar[12], sonar[13],sonar[14], sonar[15])
-        
-        
+
+
     #if (sonar[3] < 0.1) or (sonar[4] < 0.1):
         #lspeed, rspeed = +0.3, -0.5
     #elif sonar[1] < 0.3:
@@ -181,13 +208,82 @@ def avoid(sonar, blobs, coord):
     #else:
         #lspeed, rspeed = +2.0, +2.0
     #print(sonar)
-    
+
     #lspeed, rspeed = +2.0, +2.0
-        
-    print("Ahora hace: ")    
+
+    print("Ahora hace: ")
     print(lspeed,rspeed)
     return lspeed, rspeed
 
+# --------------------------------------------------------------------------
+def centerBall(coord):
+    xtarget = 0.5                   # desirable speed
+    Kp = 3                          # constant
+    error = xtarget - coord[0]      # positive is on the left, negative is on the right
+    Px = Kp * error
+
+    return Px
+
+# --------------------------------------------------------------------------
+def fixDistance(coord):
+    dist_target = 0.65               # less is more 0.6
+    Kp = 4 #5
+    dist_current = coord[2]         #( sonar[3] + sonar[4] )/ 2
+    #print('ESTA ES LA Z:', coord[2])
+    error = dist_target - dist_current
+    Pdepth = Kp * error
+
+    return Pdepth
+# --------------------------------------------------------------------------
+def avoidCollisionFront(sonar):
+    #print('CENTRAL [3]    : {}      CENTRAL [4]    : {}'.format(sonar[3], sonar[4]))
+    f_right = sonar[4]
+    f_left = sonar[3]
+    dist_target = 0.7
+
+    if f_right < 0.3 or f_left < 0.3:
+        if f_right < f_left:
+            error = dist_target - f_right
+            W = 'CD'
+        else:
+            error = dist_target - f_left
+            W = 'CI'
+        return W, error
+    else:
+        return 'OK', 0.0
+
+# --------------------------------------------------------------------------
+def avoidCollisionLat(sonar):
+
+
+    #print('IZQ [1]    : {}   '.format(sonar[1]))
+    #print('IZQ [2]    : {}   '.format(sonar[2]))
+    print('IZQ [0]    : {}   '.format(sonar[0]))
+    #print('DER [5]    : {}      DER [6]    : {}'.format(sonar[5], sonar[6]))
+    #print('____________________________________________________________')
+
+    right = sonar[6]            #sonar[4] + sonar[5] + sonar[7]
+    left = sonar[1]             #sonar[3] + sonar[2] + sonar[0]
+
+    sep_target = 0.6            #separation desired
+
+    Kp = 1 # 1
+    W = ''
+    if left < 0.24 or right< 0.24:
+
+        if right < left:
+            W = 'D'
+            error = sep_target - right
+        else:
+            W = 'I'
+            error = sep_target - left
+    else:
+        W = 'OK'
+        error = 0.0
+
+    Psep = Kp * error
+
+    return Psep, W
 # --------------------------------------------------------------------------
 
 def main():
@@ -211,15 +307,62 @@ def main():
         while sim.simxGetConnectionId(clientID) != -1:
             # Perception
             sonar = getSonar(clientID, hRobot)
-            print('getSonar')
-            # print '### s', sonar
-           # print(hRobot)
+            #print('getSonar')
 
             blobs, coord = getImageBlob(clientID, hRobot)
-            print('###  ', blobs, coord)
+            #print('###  ', blobs, coord)
 
             # Planning
-            lspeed, rspeed = avoid(sonar, blobs, coord)
+            vel_base = 0.7 #1.4 0.85
+
+            """# Sin bola --------------------------------------------
+            lspeed, rspeed = 0.0, 0.0
+            Psep, W = avoidCollisionLat(sonar)
+            if W == 'I':
+                lspeed, rspeed = vel_base - Psep, vel_base + Psep
+            else:
+                lspeed, rspeed = vel_base + Psep, vel_base - Psep
+            # Sin bola --------------------------------------------"""
+
+
+            if blobs == 1:
+                Px = centerBall(coord)
+                Pdepth = fixDistance(coord)
+                lspeed, rspeed = vel_base - Px + Pdepth,  vel_base + Px + Pdepth
+                sep_max = 0.55
+                PsepF, W = avoidCollisionFront(sonar)
+                PsepL, WL = avoidCollisionLat(sonar)
+                #vel_base = 1
+                if W == 'OK':           # si no hay choque frontal
+                    if WL != 'OK':      # si hay choque lateral
+                        if W == 'D':
+                            lspeed, rspeed = vel_base - Px + Pdepth + PsepL, 0.0  # vel_base + Px + Pdepth + Psep
+                        elif W == 'I':
+                            lspeed, rspeed = 0.0, vel_base + Px + Pdepth + PsepL  # vel_base - Px + Pdepth + Psep
+                else:
+
+                    if W == 'CD':
+                        lspeed, rspeed = vel_base - Px + Pdepth - PsepF,  0.0 #vel_base + Px + Pdepth + Psep
+                    elif W == 'CI':
+                        lspeed, rspeed = 0.0 , vel_base + Px + Pdepth - PsepF #vel_base - Px + Pdepth + Psep
+
+            else:
+                PsepF, W = avoidCollisionFront(sonar)
+                PsepL, WL = avoidCollisionLat(sonar)
+                lspeed, rspeed = +1.4, -1.4
+
+                if W == 'OK':           # si no hay choque frontal
+                    if WL != 'OK':      # si hay choque lateral
+                        if WL == 'D':
+                            lspeed, rspeed = vel_base + PsepL, 0.0  # vel_base + Px + Pdepth + Psep
+                        elif WL == 'I':
+                            lspeed, rspeed = 0.0, vel_base + PsepL  # vel_base - Px + Pdepth + Psep
+                elif W != 'OK':
+                    if W == 'CD':
+                        lspeed, rspeed = vel_base - PsepF,  0.0 #vel_base + Px + Pdepth + Psep
+                    elif W == 'CI':
+                        lspeed, rspeed = 0.0 , vel_base - PsepF #vel_base - Px + Pdepth + Psep"""
+
 
             # Action
             setSpeed(clientID, hRobot, lspeed, rspeed)
